9cb705bd8cde606fa6fad037ac13952315800dd5
Helix <hlxeasy@gmail.com>
2019-04-19 15:46:39 +0200

    Manual rebase from Spectrecoin repository back to UI repository: 0345ba
        Author: teknex <teknex@tekener.net>
        Date:   Sat Aug 4 01:24:40 2018 +0200
    
            pretty print

--- assets/js/tooltip.js
+++ assets/js/tooltip.js
@@ -1,104 +1,66 @@
-(function ($) {
+!function (t) {
     "use strict";
-
-    function init(element) {
-        if (element === $) {
-            element = $('[data-title]');
+    function e(e) {
+        return e === t && (e = t("[data-title]")),
+        void 0 === e && (e = this),
+            e.off("mouseenter").on("mouseenter", o)
+    }
+    function o(e) {
+        function o() {
+            t(window).width() < 1.5 * n.outerWidth() ? n.css("max-width", t(window).width() / 2) : n.css("max-width", 340);
+            var o = r.offset().left + r.outerWidth() / 2 - n.outerWidth() / 2
+                , i = r.offset().top - n.outerHeight() - 20;
+            o < 0 ? (o = r.offset().left + r.outerWidth() / 2 - 20,
+                n.addClass("left")) : n.removeClass("left"),
+                o + n.outerWidth() > t(document).width() ? (o = r.offset().left - n.outerWidth() + r.outerWidth() / 2 + 20,
+                    n.addClass("right")) : n.removeClass("right"),
+            o + r.outerWidth() > t(document).width() && (o = e.pageX,
+                n.removeClass("left right")),
+                i < 0 ? (i = r.offset().top + r.outerHeight() + 25,
+                    n.addClass("top"),
+                    a = !0) : n.removeClass("top"),
+                n.css({
+                    left: o,
+                    top: i
+                }).animate({
+                    top: (a ? "-" : "+") + "=10",
+                    opacity: 1
+                }, 100)
         }
-
-        if (element === undefined)
-            element = this;
-
-        return element.off('mouseenter').on('mouseenter', createTooltip)
+        function i() {
+            n.animate({
+                top: (a ? "+" : "-") + "=10",
+                opacity: 0
+            }, 100, function () {
+                t(this).remove()
+            })
+        }
+        var r = !1
+            , n = !1
+            , s = !1
+            , a = !1;
+        if (!t("input, textarea").is(":focus") && "inline-block" != t(".iw-contextMenu").css("display")) {
+            if (e.stopPropagation(),
+                t("#tooltip").remove(),
+                r = t(this),
+                s = r.attr("data-title"),
+                n = t('<div id="tooltip"></div>'),
+            !s || "" == s)
+                return !1;
+            s = s.replace(/&#013;|\n|\x0A/g, "<br />").replace(/%column%/g, function () {
+                return t(r.parents("table").find("thead tr th")[r[0].cellIndex]).text()
+            }).replace(/%([.\w\-]+),([.\w\-]+)%/g, function (t, e, o) {
+                return r.children(e).attr(o)
+            }).replace(/%([.\w\-]+)%/g, function (t, e) {
+                return r.children(e).text()
+            }),
+                n.css("opacity", 0).html(s).appendTo("body"),
+            "pointer" != r.css("cursor") && "A" != r.prop("tagName") && r.css("cursor", "help"),
+                o(),
+                t(window).resize(o),
+                r.on("contextmenu mouseleave", i)
+        }
     }
-
-    function createTooltip(event) {
-        var target = false,
-            tooltip = false,
-            data = false,
-            top = false;
-
-        if ($("input, textarea").is(':focus') || $('.iw-contextMenu').css('display') == 'inline-block')
-            return;
-
-        event.stopPropagation();
-
-        $("#tooltip").remove();
-
-        target = $(this),
-            data = target.attr('data-title'),
-            tooltip = $('<div id="tooltip"></div>');
-
-        if (!data || data == '')
-            return false;
-
-        data = data.replace(/&#013;|\n|\x0A/g, '<br />')
-
-            .replace(/%column%/g, function () {
-                return $(target.parents("table").find("thead tr th")[target[0].cellIndex]).text();
-            }).replace(/%([.\w\-]+),([.\w\-]+)%/g, function ($0, $1, $2) {
-                return target.children($1).attr($2);
-            }).replace(/%([.\w\-]+)%/g, function ($0, $1) {
-                return target.children($1).text();
-            });
-
-        tooltip.css('opacity', 0)
-            .html(data)
-            .appendTo('body');
-
-        if (target.css('cursor') != "pointer" && target.prop("tagName") != "A")
-            target.css('cursor', 'help');
-
-        function displayTooltip() {
-            if ($(window).width() < tooltip.outerWidth() * 1.5)
-                tooltip.css('max-width', $(window).width() / 2);
-            else
-                tooltip.css('max-width', 340);
-
-            var pos_left = target.offset().left + (target.outerWidth() / 2) - (tooltip.outerWidth() / 2),
-                pos_top = target.offset().top - tooltip.outerHeight() - 20;
-
-            if (pos_left < 0) {
-                pos_left = target.offset().left + target.outerWidth() / 2 - 20;
-                tooltip.addClass('left');
-            } else
-                tooltip.removeClass('left');
-
-            if (pos_left + tooltip.outerWidth() > $(document).width()) {
-                pos_left = target.offset().left - tooltip.outerWidth() + target.outerWidth() / 2 + 20;
-                tooltip.addClass('right');
-            } else
-                tooltip.removeClass('right');
-
-            if (pos_left + target.outerWidth() > $(document).width()) {
-                pos_left = event.pageX;
-                tooltip.removeClass('left right');
-            }
-
-            if (pos_top < 0) {
-                pos_top = target.offset().top + target.outerHeight() + 25;
-                tooltip.addClass('top');
-                top = true;
-            } else
-                tooltip.removeClass('top');
-
-            tooltip.css({left: pos_left, top: pos_top})
-                .animate({top: (top ? '-' : '+') + '=10', opacity: 1}, 100);
-        };
-
-        displayTooltip();
-        $(window).resize(displayTooltip);
-
-        function removeTooltip() {
-            tooltip.animate({top: (top ? '+' : '-') + '=10', opacity: 0}, 100, function () {
-                $(this).remove();
-            });
-        };
-
-        target.on('contextmenu mouseleave', removeTooltip);
-    }
-
-    $.fn.tooltip = init;
-})(jQuery);
-
-jQuery($.fn.tooltip);
+    t.fn.tooltip = e
+}(jQuery),
+    jQuery($.fn.tooltip);
