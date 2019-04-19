6b046774861b8698a5f28302b9b785cb36bdebb7
Helix <hlxeasy@gmail.com>
2019-04-19 16:51:06 +0200

    Manual rebase from Spectrecoin repository back to UI repository: 8ff69cb1
        Author: teknex <teknex@tekener.net>
        Date:   Fri Aug 10 01:42:30 2018 +0200
    
            Fix if narration is undefined (private<>public transfer), addRecipient was never called on QT bridge (QT 5.9.6)

--- assets/js/spectre.js
+++ assets/js/spectre.js
@@ -657,7 +657,7 @@ function appendTransactions(f) {
 }
 function spectreChatInit() {
   var r20 = [{
-    name : "Send&nbsp;Spectrecoin",
+    name : "Send&nbsp;Spectre",
     fun : function() {
       clearRecipients();
       $("#pay_to0").val($("#contact-list .selected .contact-address").text());
