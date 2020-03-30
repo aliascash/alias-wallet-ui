55209b9eb2a5fe321248d6821704802d8e744d50
HLXEasy <helix@spectreproject.io>
2020-03-30 14:01:15 +0200

    Some code beautification

--- fixAssets.sh
+++ fixAssets.sh
@@ -38,8 +38,7 @@ assets/js/tooltip.js
 assets/js/spectre.js
 "
 
-for file in ${MINIFY}
-do
+for file in ${MINIFY} ; do
     echo minify ${file}
     filename=${file%.*}
     extension=${file##*.}
@@ -53,13 +52,10 @@ cd build
 assets=`find assets/ -type f|sort`
 cd ..
 
-while read line
-do
+while read line ; do
     echo "$line" >> build/spectre.qrc
-    if [[ "$line" == '    <qresource prefix="/">' ]]
-    then
-        for asset in ${assets}
-        do
+    if [[ "$line" == '    <qresource prefix="/">' ]] ; then
+        for asset in ${assets} ; do
             [[ ${MINIFY} =~ $asset ]] && continue
             echo '        <file alias="'${asset}'">src/qt/res/'${asset}'</file>' >> build/spectre.qrc
         done
@@ -69,46 +65,39 @@ done < spectre.qrc
 ALIASES=()
 FILES=()
 unset IFS
-while read line
-do
+while read line ; do
     [[ "$line" == '<qresource prefix="/">' ]] && RES=true
     [[ "$line" == '</qresource>' ]] && break
-    if [[ "$RES" = true ]]
-    then
+    if [[ "$RES" = true ]] ; then
         line=`echo ${line} | sed 's^<file alias="\(.*\)">.*</file>^\1^'`
         ALIASES+=(${line})
         FILES+=("build/${line}")
     fi
 done < build/spectre.qrc
 
-for index in ${!FILES[*]}
-do
+for index in ${!FILES[*]} ; do
     file=${FILES[$index]}
     alias=${ALIASES[$index]}
-    if [[ ${file} == *".css" ]] && [[ $(fgrep "url(" ${file} -l) ]]
-    then
+    if [[ ${file} == *".css" ]] && [[ $(fgrep "url(" ${file} -l) ]] ; then
         DIR=`dirname ${alias}`
         PREVDIR=`dirname ${DIR}`
         REPLACE=$(fgrep "url(" ${file} | grep -o 'url(['\''"]\?\([^'\''")]\+\)["'\'']\?)' | sed 's/url(\|["'\'']\|)//g' | sed 's/&/\\&/g')
-        for filename in ${REPLACE}
-        do
+        for filename in ${REPLACE} ; do
             [[ ${filename} == "qrc:"* ]] && continue
             [[ ${filename} == "data:image"* ]] && continue
 
-            if [[ ${filename} == "../"* ]]
-            then
-              replacement=`echo ${filename} | sed 's!^..!qrc:///'${PREVDIR}'!'`
-              sed -i ${backupFileSuffix} 's^url(['\''"]\?'${filename}'['\''"]\?)^url('${replacement}')^g' ${file}
+            if [[ ${filename} == "../"* ]] ; then
+                replacement=`echo ${filename} | sed 's!^..!qrc:///'${PREVDIR}'!'`
+                sed -i ${backupFileSuffix} 's^url(['\''"]\?'${filename}'['\''"]\?)^url('${replacement}')^g' ${file}
             else
-              replacement="qrc:///$DIR/$filename"
-              sed -i ${backupFileSuffix} 's^url(['\''"]\?'${filename}'['\''"]\?)^url('${replacement}')^g' ${file}
+                replacement="qrc:///$DIR/$filename"
+                sed -i ${backupFileSuffix} 's^url(['\''"]\?'${filename}'['\''"]\?)^url('${replacement}')^g' ${file}
             fi
         done
         echo ${file}
     fi
 
-    if [[ ${file} == *".js" ]] && [[ $(fgrep "assets" ${file} -l) ]]
-    then
+    if [[ ${file} == *".js" ]] && [[ $(fgrep "assets" ${file} -l) ]] ; then
         sed -i ${backupFileSuffix} 's^\(assets/\(js\|icons\|img\|plugins\)\)^qrc:///\1^g' ${file}
         sed -i ${backupFileSuffix} 's^\./qrc:///^qrc:///^g' ${file}
 
