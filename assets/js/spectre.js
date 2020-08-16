a949fca7258b3df8e0ceee894277be1e0448de37
teknex <teknex@gmail.com>
2020-08-16 11:18:17 +0200

    Optimize trx list after font size changes

--- assets/js/spectre.js
+++ assets/js/spectre.js
@@ -588,7 +588,8 @@ function formatTransaction(tx) {
             "<td class='trans-status' data-value='" + o.c + "'" + ((tx.length > 1) ? " data-title='" + tooltipStatus + "'" : "") + "><i class='fa fa-2x " + o.s + "'></td>"+
             "<td class='trans-date' data-value='" + o.d + "'" + ((tx.length > 1) ? " data-title='" + tx[0].d_s + " - "+ tx[tx.length-1].d_s + "'" : "") + ">" + o.d_s + "</td>"+
             "<td class='amount' style='color:" + o.am_c + ";' data-value='" + unit.format(totalAmount) + "'>" + unit.format(totalAmount) + " " + unit.display + "</td>"+
-            "<td class='trans-type'" + ((tx.length > 1) ? " data-title='" + tooltipStatus + "'" : "") + "><img height='22px' width='22px' src='assets/svg/tx_" + o.t + ".svg' /><span>" + o.t_l + " <b>&nbsp;(x" + tx.length + ")</b></span></td>"+
+            "<td class='trans-type-icon'" + ((tx.length > 1) ? " data-title='" + tooltipStatus + "'" : "") + "><img width='100%' src='assets/svg/tx_" + o.t + ".svg' /></td>"+
+            "<td class='trans-type'" + ((tx.length > 1) ? " data-title='" + tooltipStatus + "'" : "") + ">" + o.t_l + " <b>&nbsp;(x" + tx.length + ")</b></td>"+
             "<td class='address' style='color:" + o.a_c + ";' data-value='" + addrCons + "' data-label='" + addrCons + "' data-title='" + addrCons + "' ><span>" + ((addrCons.length > 26) ? (addrCons.substr(0, 26) + "...") : addrCons) + "</span></td>"+
             "<td class='trans-nar' data-title='" + narrCons + "'>" + narrCons + "</td>" +
             "</tr>";
@@ -599,7 +600,8 @@ function formatTransaction(tx) {
             "<td class='trans-status' data-value='" + o.c + "' data-title='" + o.tt + "'><i class='fa fa-2x " + o.s + "'></td>"+
             "<td class='trans-date' data-value='" + o.d + "'>" + o.d_s + "</td>"+
             "<td class='amount' style='color:" + o.am_c + ";' data-value='" + o.am_d + "'>" + o.am_d + " " + unit.display + "</td>"+
-            "<td class='trans-type'><img height='22px' width='22px' src='assets/svg/tx_" + o.t + ".svg' /><span>" + o.t_l + "</span></td>"+
+            "<td class='trans-type-icon'><img width='100%' src='assets/svg/tx_" + o.t + ".svg' /></td>"+
+            "<td class='trans-type'><span>" + o.t_l + "</span></td>"+
             "<td class='address' style='color:" + o.a_c + ";' data-value='" + o.ad + "' data-label='" + o.ad_l + "'><span "+ ( o.ad ? "class='editable'" : "") +">" + ((o.ad_d.length > 26) ? (o.ad_d.substr(0, 26) + "...") : o.ad_d) + "</span></td>"+
             "<td class='trans-nar'>" + o.n + "</td>" +
             "</tr>";
